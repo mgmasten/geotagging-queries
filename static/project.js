03bf8482c719d49298352c9845b5bbdda12c13a0
mgmasten <matti.masten@yahoo.com>
2019-07-08 15:30:11 -0700

    Switch to OSM and Leaflet, add loader, red icons

--- static/project.js
+++ static/project.js
@@ -1,3 +1,12 @@
+var redIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 document.addEventListener("DOMContentLoaded", contentLoaded);
 
 function contentLoaded() {
@@ -7,8 +16,12 @@ function contentLoaded() {
 function submission() {
   var query = document.getElementById("query").value;
   console.log(query);
+  document.getElementById("loaderid").style.visibility = "visible";
+  document.getElementById("loadingMessage").innerHTML = "Running your query...";
   //document.getElementById("displayQuery").innerHTML = query;
   $.getJSON($SCRIPT_ROOT + '/map', {query: query}, function(data) {
+        document.getElementById("loaderid").style.visibility = "hidden";
+        document.getElementById("loadingMessage").innerHTML = "";
         plotMarkers(data);
       })
 }
@@ -18,10 +31,7 @@ function submission() {
 function plotMarkers(results) {
  for (var i = 0; i < results.locations.length; i++) {
     var coords = results.locations[i];
-    var latLng = new google.maps.LatLng(coords['lat'],coords['lon']);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
- }
+    var latLng = L.latLng(coords['lat'], coords['lon']);
+    L.marker(latLng, {icon: redIcon}).addTo(mymap);
+    };
 }
