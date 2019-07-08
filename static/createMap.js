dd406900fad9aaa2f71b60d68c176f3fd04c95b3
mgmasten <matti.masten@yahoo.com>
2019-07-08 15:29:26 -0700

    Switch to OSM and Leaflet. Create initial map

---
+++ static/createMap.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid', {
+     worldCopyJump: true,
+     maxBounds: [ [-90, -180], [90, 180]]
+}).setView([2.8, -210], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   id: 'world-map',
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+   subdomains: 'abc'
+   }).addTo(mymap);
