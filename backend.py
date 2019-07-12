02bc1c5db31b1e957ba6dd1199f5c0ccb6d7587d
mgmasten <matti.masten@yahoo.com>
2019-07-12 13:04:22 -0700

    Improve readability

--- backend.py
+++ backend.py
@@ -5,24 +5,17 @@ import requests
 import collections
 
 def get_locations(query, numResults):
-    results = []
-    pattern = re.compile('//(.*?)/')
     coordinates = []
+    pattern = re.compile('//(.*?)/') # Create pattern to capture domain
 
     for result in search(query, stop=numResults, pause=2):
-        results.append(result)
-        #print(result)
         substring = pattern.search(result)
         clean_result = result[ (substring.span()[0]+2) : (substring.span()[1] - 1) ]
-        #print(clean_result)
         ip = socket.gethostbyname(clean_result)
-        #print(ip)
         r = requests.get(url = 'https://extreme-ip-lookup.com/json/' + ip)
         coordinates.append( (r.json()['lat'], r.json()['lon']))
-        #coordinates.append( { "lat": r.json()['lat'], "lon": r.json()['lon'] } )
-        #print(r.json()['lat'], r.json()['lon'])
 
-    countedCoordinates = collections.Counter(coordinates)
+    countedCoordinates = collections.Counter(coordinates) # Count number of results at each location
     coordinates = list(countedCoordinates.keys())
     counts = list(countedCoordinates.values())
 
