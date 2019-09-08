88f386242c4d57ce5c44e704734fa4c586074243
mgmasten <matti.masten@yahoo.com>
2019-09-08 11:52:20 -0700

    Get rid of MyFlaskApp now that dictionaries are not being created upon instantiation

--- app.py
+++ app.py
@@ -3,17 +3,7 @@ import backend
 import os
 import time
 
-# Define a new subclass of app so that it can call functions upon initiation
-# The three dictionaries can be created upon server startup and held in memory until server shutdown
-class MyFlaskApp(Flask):
-    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-        if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-            with self.app_context():
-                backend.create_languages_and_tlds_dict()
-                backend.create_postalcode_dict()
-        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
-
-app = MyFlaskApp(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def search():
